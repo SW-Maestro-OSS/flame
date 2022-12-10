14743916090351e0ea6269118ed6b118aaffb793
hyunjin <leehj0110@kakao.com>
2022-12-10 23:01:14 +0900

    feat: 기본 버튼 type, style 추가

--- packages/react/src/components/Button/Button.tsx
+++ packages/react/src/components/Button/Button.tsx
@@ -1,3 +1,20 @@
-export const Button = () => {
-  return <div>hi</div>;
-};
+import { forwardRef, ReactNode } from 'react';
+import { css } from '@emotion/react';
+import styled from '@emotion/styled';
+
+type ButtonSize = 'small' | 'medium' | 'large';
+
+export interface ButtonProps {
+  /**
+   * Button의 크기를 정합니다.
+   * @type 'small' | 'medium' | 'large' | undefined
+   */
+  size?: ButtonSize;
+
+  children: ReactNode;
+}
+
+const baseStyles = () => css``;
+const StyledButton = styled('button')(baseStyles);
+
+export const Button = forwardRef(() => <StyledButton>hi</StyledButton>);
